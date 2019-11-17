921f6606be3476f5feb5ac45cf4738a879ac2759
Bryce Eklund <bryce.eklund@gmail.com>
2019-11-17 15:51:18 -0800

    provision for heroku

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     CLIENT_ORIGIN: 'https://chord-magic.now.sh/',
-    JWT_SECRET: process.env.JWT_SECRET || 'secret-code'
+    JWT_SECRET: process.env.JWT_SECRET || 'secret-code',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://bryce@localhost/chord-magic'
 }
